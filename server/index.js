87b52cc5fa60883ffb944434112ff3b94c55880c
jonnythegoat12 <jteklu2@umbc.edu>
2025-06-02 00:01:54 -0400

    Set up backend with Express and nodemon

---
+++ server/index.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('JobApplyX API is running');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+    
