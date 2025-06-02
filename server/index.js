ed76cc59577e64d825d655bbf69d79e084ee3a9e
jonnythegoat12 <jteklu2@umbc.edu>
2025-06-02 11:29:55 -0400

    connecting client/server and setting up user auth with mongo DB

--- server/index.js
+++ server/index.js
@@ -1,3 +1,12 @@
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB ✅'))
+.catch((err) => console.error('MongoDB error:', err));
+
+
+
 // index.js
 const express = require('express');
 const mongoose = require('mongoose');
@@ -11,6 +20,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//MongoDB connection
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+
 // Sample route
 app.get('/', (req, res) => {
   res.send('JobApplyX API is running');
@@ -20,4 +34,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-    
